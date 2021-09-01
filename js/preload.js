20fc0bf238cbd9fae3b42b3eb706fe067e162214
Destry Amiott <destry.amiott@gmail.com>
2021-09-01 10:24:16 -0500

    Forgot to update gallery.html

--- js/preload.js
+++ js/preload.js
@@ -13,20 +13,34 @@ function cycleImages(imageArray, elementId) {
     setInterval(toggleImages, 3000);
 }
 
-function preloadImages(imageArray, elArray, index) {
+function preloadImages(imageArray, index) {
     index = index || 0;
 
     if (imageArray && imageArray.length > index) {
+
         let img = new Image();
+        img.src = imageArray[index];
 
         img.onload = function () {
-            preloadImages(imageArray, elArray, index + 1);
+            preloadImages(imageArray, index + 1);
         }
-        img.src = imageArray[index];
 
         // used to keep the image objects in memory.
         imageMem.push(img);
     }
-
-    console.log(imageMem);
 }
+
+function enlarge(img) {
+    img.style.height = "800px";
+    img.style.width = "auto";
+    img.parentElement.parentElement.style.zIndex= "5";
+    img.style.position = "fixed"
+}
+
+function reduce(img){
+    img.style.height = "auto";
+    img.style.width = "180px";
+    img.parentElement.parentElement.style.zIndex= "-1";
+    img.style.position = "";
+}
+
